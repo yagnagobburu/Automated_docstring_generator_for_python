b306f6622780cb532cb01c75b48fe1e273e35529
yagnagobburu <yagnagobburu@gmail.com>
2026-02-06 18:15:11 +0530

    test docstring hook

--- src/sample.py
+++ src/sample.py
@@ -1,16 +1,2 @@
-"""Sample module.
-
-This module provides functions to create, read, update, and delete user records
-in the database, ensuring data integrity and validating inputs.
-"""
-
-def add(a, b):
-    """add.
-
-    add function.
-
-    Args:
-        a: Description.
-        b: Description.
-    """
-    return a + b
+def add(a,b):
+    return a+b
